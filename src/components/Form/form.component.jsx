80c2216f0405c3797aa44fe95425e60bd5012526
hyezhu <huyennhu@me.com>
2023-05-19 19:13:18 +0200

    form fixed 3rd time

--- src/components/Form/form.component.jsx
+++ src/components/Form/form.component.jsx
@@ -24,7 +24,7 @@ export default function Form() {
        fetch("/", {
          method: "POST",
          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-         body: encode({ "form-name": "contact", ...state }),
+         body: encode({ "contactForm": "contact", ...state }),
        })
          .then(() => alert("Success!"))
          .catch((error) => alert(error));
